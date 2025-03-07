0adf702e4c923eb4abffe465d36a2998505476b8
RasulAlakbarli <alakbarlirasul@gmail.com>
2025-03-07 11:11:57 +0100

    Added: problem.py

--- problem.py
+++ problem.py
@@ -5,20 +5,30 @@ from pathlib import Path
 
 from sklearn.model_selection import StratifiedShuffleSplit
 
-problem_title = 'Template RAMP kit to create data challenges'
-
-_prediction_label_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+problem_title = 'Superconductor Critical Temperature Prediction'
 
 # A type (class) which will be used to create wrapper objects for y_pred
-Predictions = rw.prediction_types.make_multiclass(
-    label_names=_prediction_label_names
-)
+Predictions = rw.prediction_types.make_regression()
 
 # An object implementing the workflow
 workflow = rw.workflows.Estimator()
 
+
+# define the mean squared error score (specific to the competition)
+class MSE(rw.score_types.BaseScoreType):
+    is_lower_the_better = True
+    minimum = 0.0
+    maximum = float('inf')
+
+    def __init__(self, name='mse', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def score_function(self, ground_truths, predictions):
+        return ((ground_truths - predictions) ** 2).mean()
+
 score_types = [
-    rw.score_types.Accuracy(name='accuracy', precision=4),
+    MSE(name='mse', precision=2),
 ]
 
 
@@ -27,22 +37,22 @@ def get_cv(X, y):
     return cv.split(X, y)
 
 
-def load_data(path='.', file='X_train.csv'):
-    path = Path(path) / "data"
-    X_df = pd.read_csv(path / file)
+def load_data(file):
+    X_df = pd.read_csv(file)
 
     y = X_df['target']
-    X_df = X_df.drop(columns=['target'])
+    # Replace None value in y by `-1
+    y = y.fillna(-1).values
 
     return X_df, y
 
 
 # READ DATA
 def get_train_data(path='.'):
-    file = 'X_train.csv'
-    return load_data(path, file)
+    train_file = Path(path) / "data" / 'X_train.csv'
+    return load_data(train_file)
 
 
 def get_test_data(path='.'):
-    file = 'X_test.csv'
-    return load_data(path, file)
+    test_file = Path(path) / "data" / 'X_test.csv'
+    return load_data(test_file)
